e46b59135d5e53a6f1abe7d649442139c39668a0
waldo ungerer <system2.0@waldos-MacBook-Air.local>
2023-10-29 12:52:34 +0200

    improved functionality

--- crawler.py
+++ crawler.py
@@ -1,81 +1,89 @@
-# Import the necessary libraries
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
+# Importing the required libraries
+import requests  # For making HTTP requests
+from bs4 import BeautifulSoup  # For parsing HTML
+from urllib.parse import urljoin, urlparse  # For URL manipulation
 
-# Define the Crawler class
+# Defining the Crawler class
 class Crawler:
-    # Initialize the crawler with the starting URL
+    # Class constructor with the starting URL as a parameter
     def __init__(self, start_url):
-        self.start_url = start_url  # The URL to start crawling from
-        self.visited_links = set()  # A set to store the URLs of the pages we've visited
-        self.index = {}  # A dictionary to serve as our index, mapping words to URLs
+        self.start_url = start_url  # Assigning the starting URL
+        self.visited_links = set()  # Initializing a set to store visited URLs
+        self.index = {}  # Initializing an empty dictionary for the index
 
-    # Define the crawl method that will visit and parse pages
+    # Method to crawl the web page
     def crawl(self, url):
-        # If we've already visited this URL, we don't need to visit it again
-        if url in self.visited_links:
+        if url in self.visited_links:  # If the URL has already been visited, return
             return
 
-        # Add this URL to our set of visited links
-        self.visited_links.add(url)
+        self.visited_links.add(url)  # Add the URL to the set of visited links
 
-        # Try to send a GET request to the URL
         try:
-            response = requests.get(url, timeout=5)  # We use a timeout of 5 seconds
-            print(f"Status code for {url}: {response.status_code}")  # Print the status code
-            response.raise_for_status()  # If the response contains an HTTP error status code (like 404), this line will raise an exception
+            response = requests.get(url, timeout=5)  # Send a GET request to the URL with a timeout of 5 seconds
+            print(f"Status code for {url}: {response.status_code}")  # Print the status code for debugging
+            response.raise_for_status()  # Raise an exception if there's an HTTP error
         except (requests.RequestException, ValueError) as e:
-            # If there was an error with the request (like a timeout, DNS resolution failure, etc.) or the URL was invalid, print an error message and return
-            print(f"Unable to fetch page {url} due to {str(e)}")
+            print(f"Unable to fetch page {url} due to {str(e)}")  # Print an error message if there's a request exception or value error
             return
 
-        # If the content type of the response is not HTML, we don't want to parse it
-        if 'html' not in response.headers['content-type']:
+        if 'html' not in response.headers['content-type']:  # If the content type is not HTML, return
             return
 
-        # Parse the HTML content of the page with BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')  # Parse the HTML content of the page
 
-        # Print out the parsed HTML content for debugging purposes
-        print(soup.prettify())
+        print(soup.prettify())  # Print the parsed HTML content for debugging
 
-        # Index this page (i.e., add its words to our index)
-        self.index_page(url, soup)
+        self.index_page(url, soup)  # Index the page
 
-        # Find all links on this page
-        links = soup.find_all('a')
+        links = soup.find_all('a')  # Find all links on the page
 
-        # For each link...
-        for link in links:
-            href = link.get('href')  # Get the destination URL of the link
+        for link in links:  # For each link on the page...
+            href = link.get('href')  # Get the href attribute of the link
 
-            # If this is a valid link...
-            if href:
+            if href:  # If there's an href attribute...
                 next_url = urljoin(url, href)  # Construct an absolute URL from this relative URL
-                
-                # If this URL is on the same domain as our starting URL...
-                if urlparse(next_url).netloc == urlparse(self.start_url).netloc:
+
+                if urlparse(next_url).netloc == urlparse(self.start_url).netloc:  # If this URL is on the same domain as our starting URL...
                     self.crawl(next_url)  # ...crawl it!
 
-    # Define a method that will add words from a page's text content to our index
     def index_page(self, url, soup):
-        words = soup.get_text().split()  # Split the text content of the page into individual words
-        
-        # For each word...
-        for word in words:
+        words = soup.get_text().split()  # Split the text content into individual words
+
+        print(f"Indexing {len(words)} words from {url}")  # Print a message indicating that this page is being indexed
+
+        for word in words:  # For each word...
             if word not in self.index:  # If this word is not already in our index...
                 self.index[word] = []  # ...initialize an empty list for it
-            
+
             self.index[word].append(url)  # Add this URL to the list of URLs associated with this word
 
-    # Define a method that will search our index for pages that contain all words in a given list
-    def search(self, words):
-        return set.intersection(*(set(self.index.get(word, [])) for word in words))  # This line returns all URLs that contain all given words
+        print(f"Indexed {len(self.index)} unique words so far")  # Print a message indicating how many unique words have been indexed so far
 
-# Create a Crawler instance and start crawling!
+    def search(self, words):
+        print("WHAT ARE YOU LOOKING FOR?")
+        result_urls = set.intersection(*(set(self.index.get(word, [])) for word in words))
+
+        if result_urls:
+            print("YOUR RESULTS:")
+            for url in result_urls:
+                print(url)
+        else:
+            print("NO RESULT, UPGRADE TO PREMIUM FOR A BETTER EXPERIENCE.")
+
+    def print_index(self):
+        for word, urls in self.index.items():  
+            print(f"{word}: {urls}")  # Print each word and its associated URLs
+
+# Create an instance of the Crawler class and start crawling!
 crawler = Crawler('https://vm009.rz.uos.de/crawl/index.html')
 crawler.crawl(crawler.start_url)
 
-# Print out URLs containing your search words!
-print(crawler.search(['word1', 'word2']))
+# Search for pages containing specified words and print out their URLs
+crawler.search(['word1', 'word2'])
+
+# Print out the entire index after all pages have been crawled.
+crawler.print_index()
+
+# User input box
+user_input = input("Enter a word to search in the dictionary: ")
+crawler.search([user_input])
